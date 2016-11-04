b20db01808194c9f77afc8543622c78f5b1e03cf
Rafael Pazini <rflpazini@gmail.com>
2016-11-04 01:05:37 -0200

    trying to depploy to heroku

--- build/generated-sources/ap-source-output/entity/Messages_.java
+++ build/generated-sources/ap-source-output/entity/Messages_.java
@@ -1,17 +1,16 @@
 package entity;
 
-import java.util.Date;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-27T12:43:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-11-04T00:51:38")
 @StaticMetamodel(Messages.class)
 public class Messages_ { 
 
     public static volatile SingularAttribute<Messages, String> msgFrom;
     public static volatile SingularAttribute<Messages, Integer> id;
     public static volatile SingularAttribute<Messages, String> msgBody;
-    public static volatile SingularAttribute<Messages, Date> msgDate;
+    public static volatile SingularAttribute<Messages, String> msgDate;
 
 }
